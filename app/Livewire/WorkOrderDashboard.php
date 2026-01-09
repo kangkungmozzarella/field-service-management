dcce34dc5d9a3804cf399cf6753f36b4d8933496
unknown <andreamicolaazwir@gmail.com>
2026-01-09 08:49:21 +0700

    Add dashboard logic using optimized query

--- app/Livewire/WorkOrderDashboard.php
+++ app/Livewire/WorkOrderDashboard.php
@@ -3,11 +3,32 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Queries\WorkOrderDashboardQuery;
 
 class WorkOrderDashboard extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $region;
+    public $status;
+
     public function render()
     {
-        return view('livewire.work-order-dashboard');
+        $query = WorkOrderDashboardQuery::base();
+
+        if ($this->region) {
+            $query->where('r.id', $this->region);
+        }
+
+        if ($this->status) {
+            $query->where('wop_last.status', $this->status);
+        }
+
+        return view('livewire.work-order-dashboard', [
+            'workOrders' => $query->paginate(10),
+        ]);
     }
 }
